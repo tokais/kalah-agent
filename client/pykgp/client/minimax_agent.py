cb389915d96b9726ee0653b841d5e03053f442da
tokais <tobias.kaiser143@gmail.com>
2025-02-28 13:45:06 +0100

    added previous move cache for move ordering in minimax

--- client/pykgp/client/minimax_agent.py
+++ client/pykgp/client/minimax_agent.py
@@ -13,25 +13,27 @@ from kgp import Board
 ETA = .8
 
 def agent(state: Board):
-    #(state, side, move) : new state
+    #(str(state), side, move) : new state
     calculated_states = {}
     
-    #(state, side, move) : (score, next player)
+    #(str(state), side, move) : (score, next player)
     evaluated_moves = {}
 
+    #(str(state), side, depth) : move
+    prev_best_move = {}
 
     def evaluate_state(state: Board, eta=.8):
         assert 0 <= eta <= 1, 'Eta must be in [0,1]'
-        player = eta * state.south + (1-eta) * sum(state.south_pits)
-        opponent = eta * state.north + (1-eta) * sum(state.north_pits)
-        return player - opponent
+        south = eta * state.south + (1-eta) * sum(state.south_pits)
+        north = eta * state.north + (1-eta) * sum(state.north_pits)
+        return south - north
 
 
-    def evaluate_move(state: Board, side, move):
-        key = (str(state), side, move)
+    def evaluate_move(state: Board, side, move, depth):
+        evaluation_key = (str(state), side, move)
         # was evaluation already calculated
-        if key in evaluated_moves:
-            return evaluated_moves[key][0]
+        if evaluation_key in evaluated_moves:
+            return evaluated_moves[evaluation_key][0]
 
         score = 0
         next_player = not side
@@ -40,9 +42,16 @@ def agent(state: Board):
         seeds = state.pit(side, move)
         landing_pit = (move + seeds) % (state.size*2 + 1)
 
-        # is there a extra move, then return high score
+        # best move already found in previos iteration, then search it first
+        best_move_key = (str(state), side, depth)
+        if best_move_key in prev_best_move and prev_best_move[best_move_key] == move:
+            score = 1000
+            if landing_pit == state.size:
+                next_player = side
+
+        # is there a extra move, then search it second
         # should result in placement in the beginning of sorted move list
-        if landing_pit == state.size:
+        elif landing_pit == state.size:
             score = 999
             next_player = side
         
@@ -52,19 +61,19 @@ def agent(state: Board):
             capture_value = state.pit(not side, opposite_pit) + 1
             score = capture_value
 
-        evaluated_moves[key] = (score, next_player)
+        evaluated_moves[evaluation_key] = (score, next_player)
         return score
     
 
-    def minimax(state: Board, side, depth, alpha=-np.inf, beta=np.inf):
-        # base case
+    def minimax(state: Board, side, max_depth, depth=0, alpha=-np.inf, beta=np.inf):
+        # base case: reached desired depth or no more moves possible
         legal_moves = state.legal_moves(side)
-        if depth == 0 or not legal_moves:
+        if depth == max_depth or not legal_moves:
             return None, evaluate_state(state, ETA)
         
-        # evaluate all possible moves and sort them as: extra move, capture seeds, rest
+        # evaluate all possible moves and sort them as: best move in previous iteration, extra move, capture seeds, rest
         # determine the next player for every move on the way
-        moves_with_scores = [(move, evaluate_move(state, side, move)) for move in legal_moves]
+        moves_with_scores = [(move, evaluate_move(state, side, move, depth)) for move in legal_moves]
         moves_with_scores.sort(key=lambda x: -x[1])
         moves = [move for move, _ in moves_with_scores]
 
@@ -80,7 +89,7 @@ def agent(state: Board):
                     calculated_states[key] = state.sow(side, move)[0]
 
                 # call minimax for next state and evaluate the result
-                _, value = minimax(calculated_states[key], evaluated_moves[key][1], depth-1, alpha, beta)
+                _, value = minimax(calculated_states[key], evaluated_moves[key][1], max_depth, depth+1, alpha, beta)
                 if value > best_value:
                     best_value = value
                     best_move = move
@@ -100,7 +109,7 @@ def agent(state: Board):
                     calculated_states[key] = state.sow(side, move)[0]
                 
                 # call minimax for next state and evaluate the result
-                _, value = minimax(calculated_states[key], evaluated_moves[key][1], depth-1, alpha, beta)
+                _, value = minimax(calculated_states[key], evaluated_moves[key][1], max_depth, depth+1, alpha, beta)
                 if value < best_value:
                     best_value = value
                     best_move = move
@@ -110,10 +119,14 @@ def agent(state: Board):
                 if alpha >= beta:
                     return best_move, best_value
 
+        # store best move to speed up following iterations
+        key = (str(state), side, depth)
+        prev_best_move[key] = best_move
+
         return best_move, best_value
 
-
-    for i in range(1, 15):
+    # iterative deepening
+    for i in range(1, 20):
         yield minimax(state, kgp.SOUTH, i)[0]
         
 
@@ -121,7 +134,8 @@ def agent(state: Board):
 
 if __name__ == "__main__":
     host = "wss://kalah.kwarc.info/socket" #if os.getenv("USE_WEBSOCKET") else "localhost"
-    token = 'c+G6YUZAjTqEkQ==kdot'
-    kgp.connect(agent, host=host, token=token, name='NotLikeUs')
+    #token = 'c+G6YUZAjTqEkQ==kdot'
+    token = 'c+G6YUZAjTqEkQ==kdot2'
+    kgp.connect(agent, host=host, token=token, name='EvenLessThanUs')
 
     
